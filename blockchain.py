37e7b31fc7e4f9579fa362679668e6e0bdcf5313
Joris van Laar <jorisvanlaar@gmail.com>
2020-02-02 23:22:41 +0100

    Added console interface + blockchain verification

--- blockchain.py
+++ blockchain.py
@@ -4,31 +4,101 @@ blockchain = []
 
 def get_last_blockchain_value():
     """ Returns the last value of the current blockchain. """
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
-    """ Append a new value, as well as the last blockchain value, to the blockchain. 
+def add_transaction(transaction_amount, last_transaction):
+    """ Append a new value and the last blockchain value, to the blockchain. 
 
     Arguments:
         :transaction_amount: The amount that should be added.
         :last_transaction: The last blockchain transaction (default [1]).
     """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     """ Returns the input of the user (a new transaction amount) as a float. """
-    return float(input('Your transaction amount please: '))
+    user_input = float(input('Your transaction amount: '))
+    return user_input
 
 
-tx_amount = get_user_input()
-add_value(tx_amount)
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
+def print_blockchain_elements():
+    for block in blockchain:
+        print(block)
+    else:               # Get executed when the loop is done
+        print('-' * 20)
 
-print(blockchain)
+
+# Verify whether the first element of the current block is equal to the complete previous block
+def verify_chain():
+    # block_index = 0   # not necessary when using range() to utilize an index in your for-loop
+    is_valid = True
+
+    for block_index in range(len(blockchain)):
+        if block_index == 0:
+            continue
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+    return is_valid
+
+    # Instead of using a custom index variable, you can use range as seen above
+    # Either way is fine
+    
+    # for block in blockchain:
+    #     if block_index == 0:
+    #         block_index += 1
+    #         continue
+    #     elif block[0] == blockchain[block_index - 1]:
+    #         is_valid = True
+    #     else:
+    #         is_valid = False
+    #         break
+    #     block_index += 1
+    # return is_valid
+
+
+menu = True
+
+while menu:
+    print('Please choose:')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain')
+    print('q: Quit')
+
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_transaction(tx_amount, get_last_blockchain_value())
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice.upper() == 'H':    # Function to manipulate the first block of the chain into a value of 2
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
+    elif user_choice.upper() == 'Q':
+        # break
+        menu = False
+    else:
+        print('Input was invalid, please pick a value from the list!')
+    if not verify_chain():              # if verify_chain() returns False -> print a message
+        print_blockchain_elements()
+        print('Invalid blockchain!')
+        break
+# gets executed when the loop is done (doesn't work when you break out of the loop)
+else:
+    print('User left')
+
+print('Done!')
